a6f4d3f621213c4c3176f1b127857e1d22939123
Kenny Lemée <mephisdante@gmail.com>
2022-06-17 18:34:19 +0200

    Ajout des champs de formulaires

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -6,6 +6,13 @@ function Login ()
         <div>
             <Headerlogin />
             <h1>Connexion</h1>
+            <form action="http://localhost:4200/" method="get">
+            <label for="email">Email</label><br/>
+            <input type="email" id="email" required /><br/>
+            <label for="password">Mot de passe</label><br/>
+            <input type="password" id="password" required /><br/>
+            <input type="submit" value="Connexion"/>
+            </form>
         </div>
     )
 };
--- src/pages/Signup/index.jsx
+++ src/pages/Signup/index.jsx
@@ -6,6 +6,13 @@ function Signup ()
         <div>
             <Headerlogin />
             <h1>Céer un compte</h1>
+            <form action="http://localhost:4200/" method="get">
+            <label for="email">Email</label><br/>
+            <input type="email" id="email" required /><br/>
+            <label for="password">Mot de passe</label><br/>
+            <input type="password" id="password" required /><br/>
+            <input type="submit" value="S'inscrire"/>
+            </form>
         </div>
     )
 };
