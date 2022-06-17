5bf073ae4ace458b06dea4b4ea98af1b22a62833
Kenny Lemée <mephisdante@gmail.com>
2022-06-17 10:33:21 +0200

    Importation des pages 'Login' & 'Signup'

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,12 @@
+
+
+function Login () 
+{
+    return (
+        <div>
+            <h1>Connexion</h1>
+        </div>
+    )
+};
+
+export default Login;
---
+++ src/pages/Signup/index.jsx
@@ -0,0 +1,11 @@
+
+function Signup () 
+{
+    return (
+        <div>
+            <h1>Céer un compte</h1>
+        </div>
+    )
+};
+
+export default Signup;
