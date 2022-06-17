4993f0940107a7c0b288b2d73f32fe7d446dc3c3
Kenny Clint <Corey_taylor_slipknot@hotmail.fr>
2022-06-17 17:04:50 +0200

    Ajout de <Headerlogin />

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,9 +1,10 @@
-
+import Headerlogin from "../../components/Headerlogin";
 
 function Login () 
 {
     return (
         <div>
+            <Headerlogin />
             <h1>Connexion</h1>
         </div>
     )
