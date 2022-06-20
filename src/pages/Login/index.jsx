2983d29b0cacd48dc67434fbeba62c7386d7698a
Kenny Lemée <mephisdante@gmail.com>
2022-06-20 15:09:04 +0200

    Finalisation des paramètres de styles des éléments

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,18 +1,52 @@
+import styled from "styled-components";
 import Headerlogin from "../../components/Headerlogin";
+
+const StyledBody = styled.div`
+display : flex;
+flex-direction : column;
+align-items : center;
+margin-top : 1em;
+`;
+
+const StyledForm = styled.form`
+width : 30em;
+margin : auto;
+& input
+{
+    height : 2.2em;
+}
+`;
+
+const PositionForm = styled.div`
+display : flex;
+flex-direction : column;
+& label
+{
+    width : 7em;
+}
+& input
+{
+    margin-bottom : 0.5em;
+}
+`;
 
 function Login () 
 {
     return (
         <div>
             <Headerlogin />
-            <h1>Connexion</h1>
-            <form action="http://localhost:4200/" method="get">
-            <label for="email">Email</label><br/>
-            <input type="email" id="email" required /><br/>
-            <label for="password">Mot de passe</label><br/>
-            <input type="password" id="password" required /><br/>
-            <input type="submit" value="Connexion"/>
-            </form>
+                <StyledBody>
+                <h1>Connexion</h1>
+                <StyledForm action="http://localhost:4200/" method="get">
+                    <PositionForm>
+                        <label for="email">Email</label>
+                        <input type="email" id="email" required />
+                        <label for="password">Mot de passe</label>
+                        <input type="password" id="password" required />
+                    </PositionForm>
+                    <input type="submit" value="Connexion"/>
+                </StyledForm>
+            </StyledBody>
         </div>
     )
 };
