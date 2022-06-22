59e272c76265c4f17e26dc28e26137abcdc91fa7
Kenny Lemée <mephisdante@gmail.com>
2022-06-22 18:19:34 +0200

    Intégration de la route 'posts' et du traitement des images téléchargées

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const userRoutes = require("./routes/user")
+const userRoutes = require("./routes/user");
+const postRoutes = require("./routes/post");
+const path = require("path");
 require("dotenv").config();
 
 mongoose.connect(process.env.BDD_URL,
@@ -13,12 +15,14 @@ mongoose.connect(process.env.BDD_URL,
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-  });
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
 
-  app.use("/api/auth", userRoutes)
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use("/api/auth", userRoutes);
+app.use("/api/posts", postRoutes);
   
-  module.exports = app;
+module.exports = app;
